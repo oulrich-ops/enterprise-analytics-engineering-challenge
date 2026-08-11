57c656f83bf91d3cb0e23c674fe37bd2dd2a1ff3
Abraham <ulrichouedraogo500@gmail.com>
2026-08-11 12:29:22 +0200

    feat(dbt): seller state name

--- dbt/models/marts/dim_seller.sql
+++ dbt/models/marts/dim_seller.sql
@@ -1,7 +1,10 @@
 select
-    {{ dbt_utils.generate_surrogate_key(['seller_id']) }} as seller_key,
-    seller_id,
-    seller_zip_code_prefix,
-    seller_city,
-    seller_state
-from {{ ref('stg_sellers') }}
+    {{ dbt_utils.generate_surrogate_key(['s.seller_id']) }} as seller_key,
+    s.seller_id,
+    s.seller_zip_code_prefix,
+    s.seller_city,
+    s.seller_state,
+    bs.state_name as seller_state_name
+
+from {{ ref('stg_sellers') }} s
+left join {{ ref('br_states') }} bs on bs.state_code = s.seller_state
