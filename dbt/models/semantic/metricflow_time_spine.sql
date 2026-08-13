6c90c087a64de72ee784f6f2aba5acdd6b64ea6e
Abraham <ulrichouedraogo500@gmail.com>
2026-08-13 16:38:27 +0200

    feat(semantic): add MetricFlow POC with basic order metrics
    Educational POC only, does not cover all metrics

---
+++ dbt/models/semantic/metricflow_time_spine.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized = 'table',
+    )
+}}
+
+with spine as (
+    {{
+        dbt.date_spine(
+            'day',
+            "cast('2016-01-01' as date)",
+            "cast(date_add(day, 1, current_date) as date)"
+        )
+    }}
+)
+
+select cast(date_day as date) as date_day
+from spine
